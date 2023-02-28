9aab305148507356d1690dc3fabf1fcfdafcee6f
adhithya33 <126141050+adhithya33@users.noreply.github.com>
2023-02-28 10:16:37 +0530

    Add files via upload

---
+++ cricket-rate.R
@@ -0,0 +1,16 @@
+install.packages("remotes")
+install.packages("PKS")
+library(rvest)
+library(dplyr)
+link <-'https://www.icc-cricket.com/rankings/mens/player-rankings/odi/batting'
+# allowability
+web <-read_html(link)
+cricketer_name <- web %>% html_nodes (".rankings-block__banner--name-large , .name a") %>% html_text()
+View(cricketer_name)
+cricket_team<- web %>% html_nodes (".table-body__logo-text , .rankings-block__banner--nationality") %>% html_text()
+View(circket_team)
+ratings <- web %>% html_nodes (".rating , .rankings-block__banner--rating") %>% html_text()
+View(ratings)
+mens_batting_rate <- data.frame(cricketer_name, cricket_team, ratings)
+View(mens_batting_rate)
+write.csv(mens_batting_rate, 'cricket team dataset.csv')
